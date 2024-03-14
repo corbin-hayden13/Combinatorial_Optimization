a929bea00b26212d3fca739e6438225acaf1dc76
Xerminator13 <52582665+Xerminator13@users.noreply.github.com>
2024-03-14 11:43:39 -0600

    cleaning Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 from GA_CO import GAOptimizer
-from RL_CO import RLOptimizer, scaled_sigmoid
-from CO_Optimizer import grain_flow_path, wheat_supply_path, evaluate_individual, optimize_init_solution
+from CO_Optimizer import grain_flow_path, wheat_supply_path, evaluate_individual, optimize_init_solution, scaled_sigmoid
 import numpy as np
 
 
@@ -71,74 +70,6 @@ def test_optimize_init_state():
     optimize_init_solution(test_individual, 7, 0)
 
 
-def initial_main():
-    optimizer = GAOptimizer()
-    optimizer.import_data(wheat_supply_path, workbook="EnviroSpec Vision data Table", header_row=2)
-    optimizer.vectorize_column(col=37)
-
-    """include_columns =  [14, 20, 23, 24, 25, 26, 27, 28, 33, 36, 37]
-    weights = np.array([ 1, -1, -1, -1, -1, -1, -1, -1,  1,  1, -1])  # Negative weights for negative columns
-
-    dataframe = load_data(wheat_supply_path, "EnviroSpec Vision data Table")  # Row 2 is column header
-    lot_data = parse_column_unique_labels(dataframe, 3, include_columns=include_columns)
-    final_scores = score_data(lot_data, weights=weights, precision=5, weighted_average=False)
-
-    for key, item in final_scores.items():
-        print(f"Lot {key}, Score: {item}")"""
-
-
-def combo_optimization():
-    rl_opt = RLOptimizer(max_column=41)
-    ga_opt = GAOptimizer(max_column=41)
-    rl_opt.import_data(wheat_supply_path, workbook="EnviroSpec Vision data Table", header_row=2)
-    best_rl_individual, params = rl_opt.predict(num_steps=100,
-                                                hyper_parameters={"return_default_params": True})
-
-    hyper_parameters = {
-        "max_lots": params["max_lots"],
-        "row_vals": params["row_vals"],
-        "target_vals": params["target_val"],
-        "number_generations": 50,
-        "verbose": params["verbose"],
-    }
-
-    best_individual = ga_opt.optimize_from(best_rl_individual, hyper_parameters=hyper_parameters)
-    print(f"**  Best Individual: {evaluate_individual(best_individual, params['row_vals'])}")
-
-
-def test_combo_optimization():
-    rl_opt = RLOptimizer(max_column=41)
-    ga_opt = GAOptimizer(max_column=41)
-    # rl_opt.load_test_data(test_size=25, min_bound=-22000, max_bound=11000, percent_negative=0.5)
-    rl_opt.import_data(wheat_supply_path, workbook="EnviroSpec Vision data Table", header_row=2)
-    hyper_parameters = {
-        "return_default_params": True,
-        "max_lots": 6,
-        "file_name": "rl_300_test_-25000+25000_0.5_ga_compared",
-    }
-    best_rl_individual, params = rl_opt.predict(num_steps=16,
-                                                hyper_parameters=hyper_parameters)
-
-    print(f"**  RL Individual: {best_rl_individual}")
-    print(f"**  RL Solution:   {evaluate_individual(best_rl_individual, params['row_vals'])}")
-
-    hyper_parameters = {
-        "max_lots": params["max_lots"],
-        "row_vals": params["row_vals"],
-        "target_val": params["target_val"],
-        "number_generations": 25,
-        "verbose": params["verbose"],
-        "individual_mutation_chance": 0.3,
-        "gene_mutation_chance": 0.2,
-    }
-
-    best_individual = ga_opt.optimize_from(best_rl_individual, hyper_parameters=hyper_parameters)
-    print(f"**  Best GA Individual: {evaluate_individual(best_individual, params['row_vals'])}")
-
-
-# file format as: "EnviroSpec vision EIM yyyy-mm-dd.csv"
-# conversion of data (cannot protect fancy math)
-# solving a problem (the AI combinatorial optimization problem) can be protected
 if __name__ == "__main__":  # Indexed at 1 for col and row
     # combo_optimization()
     # test_combo_optimization()
